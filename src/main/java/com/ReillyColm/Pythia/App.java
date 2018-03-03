1680bad9b94ccf8303f20d84db332ccad8b7ef18
Realcolm <realcolm@gmail.com>
2018-03-03 13:19:30 +0000

    database added dev -v4

--- src/main/java/com/ReillyColm/Pythia/App.java
+++ src/main/java/com/ReillyColm/Pythia/App.java
@@ -1,5 +1,4 @@
 package com.ReillyColm.Pythia;
-
 import	java.io.File;
 import	java.io.IOException;
 import	java.util.Arrays;
@@ -12,8 +11,15 @@ import	org.apache.logging.log4j.core.config.Configurator;
 import	joptsimple.OptionException;
 import	joptsimple.OptionParser;
 import	joptsimple.OptionSet;
+import	java.sql.Connection;
+import	java.sql.DriverManager;
+import	java.sql.ResultSet;
+import	java.sql.SQLException;
+import	java.sql.Statement;
 /*****************************************************************
 	*
+	*	 Date:	2017
+	*	 @author	COR
 	*
 	*	
 	*	The	purpose	of	this	application	is	to	provide	an	example	for	the	following:
@@ -32,6 +38,7 @@ public	class App
 								//	To	view	the	arguments	being	entered
 								seeCommandlineInput(args);
 												
+								//	To	instantiate	App	class	based	in	the	parameters	entered	at	the	commandline
 								actionCommandlineInput(args);
 				}
 				
@@ -42,11 +49,20 @@ public	class App
 				private	 Scanner	someInput;
 				private	Date	today;
 				
-				//	This	is	added	to every class	that	needs	to	log	with	one	change
-				//	The	getLogger(	)	part should	contain	the	name	of	the	class	its	in
+				//	This	is	added	to	every	class	that	needs	to	log	with	one	change
+				//	The	getLogger(	)	part	should	contain	the	name	of	the	class	its	in
 				private	static	Logger	LOG;
 				
-				//	CONSTRUCTORS				
+				private	static String	VERSION	=	"0.4";
+				
+				//The	URL	and	name of	the	SQLite	database
+				//	TODO:	Remove	database	location	and	name	hard	coding	and	pass	in	as	a	parameter	in	the	next	version
+				
+				private String	databaseFile = "jdbc:sqlite:database/oreallyoreilly.db";
+				
+				//	CONSTRUCTORS
+				//............................................................
+				
 				public	App(	Level	logLevel	)
 				{	 	
 								//associate	logging	with	this	class	so	know	the	messages	that	came	from	objects	of	this	class
@@ -57,15 +73,15 @@ public	class App
 								LOG.info("Commandline	requested	log	level:"	+	logLevel	);	 	
 								LOG.info("Application	started	with	log	level	debug:"	+	LOG.isDebugEnabled());
 								
-								//test	the	logging
-								testLogOutput();
-								
+								//test	the	logging	-	uncomment	if	needed
+								//testLogOutput();
+																
 								this.someInput	=	new	Scanner(System.in);
 								
-								//do	something	here
-								System.out.println("	\n	Soon	...	stuff	will	happen	here");	 	
+								//do	something	here:	Display	the	list	of	users	from	the	database
+								showListOfUsers();
 								
-								//pause before	exit	(this	is	only	useful	if	an	error	occurs)
+								//pause	before	exit	(this	is	only	useful	if	an	error	occurs)
 								System.out.println("	\n	Press	enter	to	exit	the	program");
 								this.someInput.nextLine();
 								//close	the	program	without	error
@@ -76,9 +92,67 @@ public	class App
 				{
 								this(	Level.INFO	);
 				}
-								
+				
+				
 				//	METHODS	used	by	main()	or	debug	methods	-	note	they	are	static	methods
 				//............................................................
+				
+				/**
+					*	write	out	the	users	in	a	users	table	for	the	database	specified
+					*	
+					*/
+				private	void	showListOfUsers()
+				{
+								this.today	=	new	Date();
+								LOG.debug("Getting	list	of	Users	from	Database	as	of	"	+	today);
+								
+								//if	log	level	id	debug	e.g.	-v	parameter	used	then	show	database	file	being	used
+								LOG.debug("Database	file:"	+	this.databaseFile);
+								
+								//	Get	JDBC	connection	to	database
+								Connection	connection	=	null;
+								
+								try
+								{
+									 		//	create	a	database	connection
+									 		connection	=	DriverManager.getConnection(	this.databaseFile);
+									
+										Statement	statement	=	connection.createStatement();
+										statement.setQueryTimeout(30);		//	set	timeout	to	30	sec.
+										
+										//	Run	the	query
+										
+										ResultSet	resultSet	=	statement.executeQuery("select	*	from	user");
+										
+										//	iterate	through	the	results	create	User	objects	put	in	the	ListArray
+										
+										while(resultSet.next())
+										{
+														LOG.debug(	"User	found:	"	+	resultSet.getString("userName")	);
+										}
+									 		
+								}
+								catch(SQLException	e)
+								{
+										//	if	the	error	message	is	"out	of	memory",
+										//	it	probably	means	no	database	file	is	found
+										LOG.error(e.getMessage());
+								}	
+								finally
+								{
+										try
+										{
+												if(connection	!=	null)
+														connection.close();
+										}
+										catch(SQLException	e)
+										{
+												//	connection	close	failed.
+												LOG.error(e.getMessage());
+										}
+								}
+								
+				}//EOM
 				
 				/**
 					*	action	the	arguments	presented	at	the	command	line
@@ -90,23 +164,25 @@ public	class App
 												{	
 																final	OptionParser	optionParser	=	new	OptionParser();
 																
+																//define	the	allowed	arguments
 																optionParser.acceptsAll(Arrays.asList("v",	"verbose"),	"Set	logging	level	to	DEBUG	to	see	all	levels	of	log	messages").forHelp();
 			
-																optionParser.acceptsAll(Arrays.asList("h",	"help"), "Display help/usage information").forHelp();
-																optionParser.acceptsAll(Arrays.asList("r",	"version"),	"Display program version information").forHelp();	
+																optionParser.acceptsAll(Arrays.asList("h",	"help"),	"Display	help/usage	information").forHelp();
+																optionParser.acceptsAll(Arrays.asList("r",	"version"),	"Display	program	version	information").forHelp();
+																
 																final	OptionSet	options	=	optionParser.parse(args);
 																
-														        if (options.has("help") )
+																if	(options.has("help"))
 																{
-																				System.out.println("This program takes an SQL database with	a User table as displays the users.");
-																				System.out.println("It is provided as an example for teaching Java programming.");
+																				System.out.println("This	program	takes	an	SQL	database	with	a	User	table	as	displays	the	users.");
+																				System.out.println("It	is	provided	as	an	example	for	teaching	Java	programming.");
 																				printUsage(optionParser);
 																				System.exit(0);
 																}
 																
 																if	(options.has("version"))
 																{
-																				System.out.println("Pythia	version	0.3");
+																				System.out.println("Pythia	version	:	"	+	VERSION);
 																				System.exit(0);
 																}
 																
@@ -114,18 +190,18 @@ public	class App
 															if	(options.has("verbose")	)
 															{
 																			Level	logLevel	=	Level.DEBUG;
-																			System.out.println("RUN	WITH:	logging level	requested:	"	+	logLevel);
-																			App anApp	=	new	App(logLevel);
+																			System.out.println("RUN	WITH:	logging	level	requested:	"	+	logLevel);
+																			App	anApp	=	new	App(logLevel);
 															}
 															else
 															{
-																System.out.println("RUN	WITH: logging level	requested:	\"	+	Level.INFO");
-																App	anApp	=	new	App();
+																			System.out.println("RUN	WITH:	logging	level	requested:	"	+	Level.INFO);
+																			App	anApp	=	new	App();
 															}
 												}
 												catch	(OptionException	argsEx)
 												{
-													System.out.println("ERROR:	Arguments\\parameter	is	not	valid.	"	+	argsEx);
+													 	 System.out.println("ERROR:	Arguments\\parameter	is	not	valid.	"	+	argsEx);
 												}
 					}//EOM
 					
